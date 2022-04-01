9b04e8c40c70f903e1df1b240bd0629dd1661923
Can Kabak <ca741kab@htwg-konstanz.de>
2022-04-01 09:02:22 +0200

    Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,15 +5,15 @@
 val eol = sys.props("line.separator")
 
 val eolA = 2
-val cellnumber = 5
-val playernumber = 2
-val housenumber = 7
+val cellnumber = 2
+val playernumber = 3
+val housenumber = 2
 
 def cell(Cell: Int = cellnumber, Player: Int = playernumber) =
   ("x" + ("_" * Cell * playernumber)) * Player + eol * eolA
 
 def house(Amount: Int = housenumber, Player: Int = playernumber) =
-  (("C") * Amount + "  ") * Player + eol * eolA
+  (("H") * Amount + "  ") * Player + eol * eolA
 
 def finish(Amount: Int = housenumber, Player: Int = playernumber) =
   (("-") * Amount + "  ") * Player + eol * eolA
