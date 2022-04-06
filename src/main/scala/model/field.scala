1fdc1e225ae8c5de7dc832bc49545632e8a8c0f5
clx48 <ue981sim@htwg-konstanz.de>
2022-04-06 12:22:41 +0200

    fieldset

--- src/main/scala/model/field.scala
+++ src/main/scala/model/field.scala
@@ -1,7 +1,7 @@
 package model
 
-case class field() {
-
+case class field(value: Int) {
+  def isSet: Boolean = value != 0
   // def cell(Cell: Int = cellnumber, Player: Int = playernumber) =
   // ("x" + ("_" * Cell * playernumber)) * Player + eol * eolA
 }
