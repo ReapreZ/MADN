e29399d1b62b38c39b185892de1bcbc35d4f9e7c
clx48 <ue981sim@htwg-konstanz.de>
2022-04-20 10:40:27 +0200

    tui

--- src/main/scala/aview/tui.scala
+++ src/main/scala/aview/tui.scala
@@ -1,7 +1,15 @@
 package aview
 
-import model.{Mesh,Field,House,Player}
+import model.{Mesh, Field, House, Player}
 
 class tui {
-  
+
+  def processInputLine(input: String, mesh: Mesh): Mesh = {
+    input match {
+      case "m" => mesh
+      case "n" => new Mesh(2, 2, 2)
+
+    }
+  }
+
 }
