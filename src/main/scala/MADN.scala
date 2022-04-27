af6e59a65cfe89cd1478053af09d4e20ccb8314c
Can Kabak <ca741kab@htwg-konstanz.de>
2022-04-27 12:17:23 +0200

    tuiupdate

--- src/main/scala/MADN.scala
+++ src/main/scala/MADN.scala
@@ -9,15 +9,17 @@ object MADN {
     def main(args: Array[String]): Unit = {
         var input: String = ""
         val tui = new Tui
-        val mesh1: Mesh = (tui.startgame())
+        val mesh1: Mesh= (tui.startgame())
         while (input != "q")
             input = readLine()
             val output = tui.processInputLine(input)
             if(output == 0)
-                println("Game over!")
+                println("Game over!\n")
+            else if(output == -1)
+                println("Wrong input")
             else
                 println("\nYou rolled a " + output + "\n")
             val mesh = tui.checkinput(input, output, mesh1)
-            println(mesh)
+            println(mesh.mesh())
     }
 }
--- src/main/scala/aview/Tui.scala
+++ src/main/scala/aview/Tui.scala
@@ -6,7 +6,6 @@ import scala.io.StdIn.readLine
 class Tui {
     
     val dice1 = new Dice
-    var input: String = ""
     var playerturn = 1
     var playeramount = 1
 
@@ -15,37 +14,39 @@ class Tui {
         input match {
             case "r" => dice1.diceRandom()   
             case "q" => return 0
+            case _ => return -1
         }
     }
 
     def startgame(): Mesh = {
         println("Amount of Players:")
-        input = readLine()
-        val playeramount = getTurnI(input)
+        val input = readLine()
+        playeramount = getTurnI(input)
         println("Amount of Houses:")
         val houseamount = readLine()
         println("Amount of Cells per Player:")
         val cellamount = readLine()
         var mesh1 = Mesh(cellamount.toInt, playeramount.toInt, houseamount.toInt)
+        println(mesh1.mesh())
         println("Press 'r' to roll the dice\n")
         mesh1
     }
 
     def getOut(rolledDice: Int, mesh: Mesh): Mesh = {
-        //mesh = move(rolledDice, mesh)
+        val mesh1 = move(rolledDice, mesh)
         if(rolledDice == 6) {
-            val nextPlayer = mesh.field1.Player * mesh.field1.Cell + 1
+            val nextPlayer = mesh1.field1.Player * mesh1.field1.Cell + 1
             //println("Player " + mesh1.house1.houses(playerturn) + " can roll the dice once more\n")
             //println("Player A can move out one Piece\n")
-            println("You can roll again")
+            println("You can roll again\n")
             playerturn match {
-                case 1 => mesh.field1.cArr(0) = 'A'
+                case 1 => mesh1.field1.cArr(0) = 'A'
                     //mesh1.house1.hArr(0) = 'H'
-                case 2 => mesh.field1.cArr(nextPlayer) = 'B'
+                case 2 => mesh1.field1.cArr(nextPlayer) = 'B'
                     //mesh1.house1.hArr(4) = 'H'
-                case 3 => mesh.field1.cArr(nextPlayer * 2) = 'C'
+                case 3 => mesh1.field1.cArr(nextPlayer * 2) = 'C'
                     //mesh1.house1.hArr(8) = 'H'
-                case 4 => mesh.field1.cArr(nextPlayer * 3) = 'D'
+                case 4 => mesh1.field1.cArr(nextPlayer * 3) = 'D'
                     //mesh1.house1.hArr(12) = 'H'
             }
         } else if(rolledDice != 6) {
@@ -53,24 +54,24 @@ class Tui {
                 playerturn = 1
             else playerturn += 1
         }
-        mesh
+        mesh1
     }
 
     def checkinput(input: String, rolledDice: Int, mesh: Mesh): Mesh = {
         if (input == "r") {
             val playerturnC = getTurnC(playerturn)
-            println("It is Player" + playerturnC + "'s turn\n")
+            println("It is Player " + playerturnC + "'s turn\n")
             return getOut(rolledDice, mesh)
         }
         mesh
     }
 
-    def move(output: Int, mesh1: Mesh): Mesh = {
+    def move(rolledDice: Int, mesh1: Mesh): Mesh = {
         val playerTurnC = getTurnC(playerturn)
-        var out = mesh1.field1.cArr.indexOf(playerTurnC)
+        val out = mesh1.field1.cArr.indexOf(playerTurnC)
         if(out != -1)
             mesh1.field1.cArr(out) = ('-')
-            mesh1.field1.cArr(out + output) = (playerTurnC) 
+            mesh1.field1.cArr(out + rolledDice) = (playerTurnC) 
         mesh1   
     }
 
