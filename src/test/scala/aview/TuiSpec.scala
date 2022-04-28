7b046fb01d816ffbaf9f277fd7b0f6164084d227
Can Kabak <ca741kab@htwg-konstanz.de>
2022-04-28 13:48:32 +0200

    Specs

--- src/test/scala/aview/TuiSpec.scala
+++ src/test/scala/aview/TuiSpec.scala
@@ -6,17 +6,8 @@ import matchers.should.Matchers
 import wordspec.AnyWordSpec
 
 class TuiSpec extends AnyWordSpec with Matchers{
-  "A TUI" when {
-    var mesh1 = new Mesh(0,0,0)
-    var playernumber = 1
-    var playerturn = 1
-    val dice1 = new Dice
-    var input: String = ""
-    "it has 1 Player" should {
+    "A TUI" when {
         val tui = new Tui()
-        "start a game" in {
-            //tui.playernumber should equal (1)
-        }
         "processInputLine" should {
             tui.processInputLine("q") should equal (0)
             tui.processInputLine("r") should be < 7
@@ -29,5 +20,4 @@ class TuiSpec extends AnyWordSpec with Matchers{
             tui.startgame() shouldBe (mesh1)
         }
     }
-  }
 }
